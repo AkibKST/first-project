c8a34b5240f127c3d4e360be788a7243bc03dc88
AkibKST <akibkst22@gmail.com>
2024-05-24 00:37:11 +0600

    Added interface for student

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  matherName: string;
+  matherOccupation: string;
+  matherContactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female'; // union literal type
+  email: string;
+  contactNo: string; // for counting zero(0)
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'inActive';
+};
