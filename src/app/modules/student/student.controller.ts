bf0b35f40988232ba334bf01e519797d84246ccb
AkibKST <akibkst22@gmail.com>
2024-05-25 10:02:44 +0600

    learn get all student, get single student, create a student

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,10 +3,10 @@ import { StudentServices } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
+    const { student: studentData } = req.body; // name alias
 
     // will call service func to send this data
-    const result = await StudentServices.createStudentIntoDB(student);
+    const result = await StudentServices.createStudentIntoDB(studentData);
 
     // send response
     res.status(200).json({
@@ -19,6 +19,39 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentFromDB();
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 };
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,11 +1,26 @@
 import { Student } from './student.interface';
 import { StudentModel } from './student.model';
 
+// ekta student made korlam
 const createStudentIntoDB = async (student: Student) => {
   const result = await StudentModel.create(student);
   return result;
 };
 
+// all student database thake niye aslam
+const getAllStudentFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+// get a single student with param id
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await StudentModel.find({ id });
+  return result;
+};
+
 export const StudentServices = {
   createStudentIntoDB,
+  getAllStudentFromDB,
+  getSingleStudentFromDB,
 };
