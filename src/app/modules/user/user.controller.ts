1fe816fdd6b712411b077356d5a7b9f0465c069d
AkibKST <akibkst22@gmail.com>
2024-12-02 17:07:47 +0600

    Implement Your Army Middleware

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,35 +1,28 @@
-import { RequestHandler } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { password, student: studentData } = req.body; // name alias
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body; // name alias
 
-    // const zodParseData = StudentValidationSchema.parse(studentData);
+  // const zodParseData = StudentValidationSchema.parse(studentData);
 
-    // will call service func to send this data
-    const result = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    );
+  // will call service func to send this data
+  const result = await UserServices.createStudentIntoDB(password, studentData);
 
-    // send response
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is created successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  // send response
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is created successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
+//validate middleware
+const shenaBahini = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  next();
+};
+
 //will call controller func
-router.post('/create-student', UserControllers.createStudent);
+router.post('/create-student', shenaBahini, UserControllers.createStudent);
 
 export const UserRoutes = router;
