6fdfb630991a2df9d297ed4a670987daa810bb53
AkibKST <akibkst22@gmail.com>
2024-12-07 18:07:25 +0600

    Handle department validation when creating & updating document

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,32 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// pre save middleware
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExits = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExits) {
+    throw new Error('This department is already exits!');
+  }
+
+  next();
+});
+
+//query middleware
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExits = AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExits) {
+    throw new Error('This department does not exits!');
+  }
+
+  next();
+});
+
 // create model of mongoose by schema
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
