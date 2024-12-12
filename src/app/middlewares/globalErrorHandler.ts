1d0961c12d88123e7319ef756abd0c0fcc8dd1ed
AkibKST <akibkst22@gmail.com>
2024-12-12 22:33:55 +0600

    How to handle castError and 11000 error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,8 @@ import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import { TErrorSources } from '../interface/error';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   //setting default values
@@ -29,33 +31,16 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
-    // } else if (err?.name === 'CastError') {
-    //   const simplifiedError = handleCastError(err);
-    //   statusCode = simplifiedError?.statusCode;
-    //   message = simplifiedError?.message;
-    //   errorSources = simplifiedError?.errorSources;
-    // } else if (err?.code === 11000) {
-    //   const simplifiedError = handleDuplicateError(err);
-    //   statusCode = simplifiedError?.statusCode;
-    //   message = simplifiedError?.message;
-    //   errorSources = simplifiedError?.errorSources;
-    // } else if (err instanceof AppError) {
-    //   statusCode = err?.statusCode;
-    //   message = err.message;
-    //   errorSources = [
-    //     {
-    //       path: '',
-    //       message: err?.message,
-    //     },
-    //   ];
-    // } else if (err instanceof Error) {
-    //   message = err.message;
-    //   errorSources = [
-    //     {
-    //       path: '',
-    //       message: err?.message,
-    //     },
-    //   ];
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   //ultimate return
