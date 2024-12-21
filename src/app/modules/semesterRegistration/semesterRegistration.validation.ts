16436df684c1bbf5e696b8eb3c621e845c364651
AkibKST <akibkst22@gmail.com>
2024-12-21 18:46:45 +0600

    Create ER Diagram of Semester Registration and Boiller Plate

---
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const createSemesterRegistrationValidationSchema = z.object({
+  body: z.object({}),
+});
+
+const updateSemesterRegistrationValidationSchema = z.object({
+  body: z.object({}),
+});
+
+export const SemesterRegistrationValidations = {
+  createSemesterRegistrationValidationSchema,
+  updateSemesterRegistrationValidationSchema,
+};
