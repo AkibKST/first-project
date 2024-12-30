39beb0aa1f3353e9357d3ca22d27ad67649a2e86
AkibKST <akibkst22@gmail.com>
2024-12-30 15:30:50 +0600

    Implement authorization using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -3,9 +3,10 @@ import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
 
 //auth  middleware
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     //if the token is sent from client
     const token = req.headers.authorization;
@@ -25,6 +26,14 @@ const auth = () => {
         if (err) {
           throw new AppError(401, 'You are not authorized!');
         }
+
+        // check if requiredRoles & decoded roles are valid
+
+        const role = (decoded as JwtPayload).role;
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(401, 'You are not authorized!');
+        }
+        //--------------------------------------------------
 
         //decoded
         req.user = decoded as JwtPayload;
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,12 +4,15 @@ import { studentValidations } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
 import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 //will call controller func
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
