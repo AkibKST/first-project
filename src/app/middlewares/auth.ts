37a6e87101d822f264c1513f64861e2a156d50bf
AkibKST <akibkst22@gmail.com>
2024-12-30 15:04:38 +0600

    Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,16 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
-//validate middleware
+//auth  middleware
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    //if the token is sent from client
     const token = req.headers.authorization;
 
     if (!token) {
       throw new AppError(401, 'You are not authorized!');
     }
-    next();
+    //-----------------------------------------------------
+
+    //check if the token is valid
+
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        //err
+        if (err) {
+          throw new AppError(401, 'You are not authorized!');
+        }
+
+        //decoded
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
+    //----------------------------------------
   });
 };
 
